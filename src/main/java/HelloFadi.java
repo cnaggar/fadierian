e7d4d92e79561914ca43c7adb0b3941b1ef5e216
Cherif Naggar <cherif.naggar@tritondigital.com>
2016-06-12 18:59:55 -0400

    Initital commit

---
+++ src/main/java/HelloFadi.java
@@ -0,0 +1,10 @@
+/**
+ * Created by cnaggar on 2016-06-12.
+ */
+public class HelloFadi {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello Fadi");
+    }
+    
+}
